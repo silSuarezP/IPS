a7f36ed853002603e3ecd708ad50c6fd4e663d50
uo277412 <uo277412@uniovi.es>
2023-09-27 12:26:55 +0200

    cambio 3 mars en conflicto

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -5,7 +5,7 @@ package demo;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("línea 1");	
-		System.out.println("línea 2");	
+		System.out.println("cambio 3 mars en conflicto");	
 		System.out.println("línea 3");	
 	}
 /**cambio desde mars**/
