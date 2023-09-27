2efb6225cb58efcbd920d0db7666e4726c4d9a1a
uo277412 <uo277412@uniovi.es>
2023-09-27 12:05:48 +0200

    cambio y borrado

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -3,7 +3,7 @@ package demo;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("línea 1");	
-		System.out.println("línea 1");	
-		System.out.println("línea 1");	
+		System.out.println("línea 2");	
+		System.out.println("línea 3");	
 	}
 }
